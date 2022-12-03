288835d35de3a243b75fdb858ec41f577ec433a7
Lucas Amaral <koalasoftbr@gmail.com>
2022-12-03 19:16:20 -0300

    init

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
+console.log('Criado Por Lucas Amaral');
+console.log('Contato: https://www.linkedin.com/in/lucas-amaral-a99737162/');
